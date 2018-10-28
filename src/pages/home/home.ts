e873fc5189aba0100e4ac919d4e63a476f426cf1
Carlos <admin@MacBook-Pro-de-Admin.local>
2018-10-28 17:00:36 -0300

    Inicio do desenvolvimento da interface e da logica dos botoes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NativeAudio } from '@ionic-native/native-audio';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,10 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public nativeAudio: NativeAudio) {
   }
-
+   tocarSom = () => {
+   this.nativeAudio.preloadComplex('somPraFora',"../../assets/sounds/somFora.mp3",1,1,0); 
+   this.nativeAudio.play('somPraFora');
+   }
 }
