8c7059ea9c2c90d3af6c998f6310f4863bc9af2d
Helbert <helbertnunes97@gmail.com>
2018-10-28 22:26:15 -0300

    Alteracoes na tela e implementacao de estilo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,9 +9,25 @@ import { NativeAudio } from '@ionic-native/native-audio';
 export class HomePage {
 
   constructor(public navCtrl: NavController, public nativeAudio: NativeAudio) {
+  this.nativeAudio.preloadComplex('somPraFora','../../assets/sounds/somFalta.mp3',1,1,0);
+   
+  this.nativeAudio.preloadSimple('somFalta','../../assets/sounds/somFalta.mp3'); 
   }
    tocarSom = () => {
-   this.nativeAudio.preloadComplex('somPraFora',"../../assets/sounds/somFora.mp3",1,1,0); 
-   this.nativeAudio.play('somPraFora');
+   this.nativeAudio.play('somPraFora').then(()=>{console.log('tocando');})
    }
+   tocarSomTorcida = () => {
+    this.nativeAudio.preloadComplex('somTorcida','/assets/sounds/somTorcida.mp3',1,0,0);
+    this.nativeAudio.play('somTorcida').then(()=>{console.log('tocando');})
+    }
+   tocarSomAlter = () => {
+    this.nativeAudio.play('somFalta').then(()=>{console.log('tocando');})
+    }
+   tocarSomParametro = (caminho) => {
+    this.nativeAudio.preloadComplex('somPraFora',caminho,1,1,0); 
+    this.nativeAudio.play('somPraFora');
+    }
+    colocaLog = () =>{
+      console.log('teste');
+    }
 }
